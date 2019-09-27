ae722fff2df649e439648cb9eda6ee818502999f
Luiz Henrique Naspolini <42879594+luiznds62@users.noreply.github.com>
2019-09-27 20:56:11 -0300

    Validação de Token da questão realizada

--- src/services/QuestaoService.js
+++ src/services/QuestaoService.js
@@ -1,86 +1,102 @@
 const ResponseBuilder = require("../common/Response");
+const md5 = require('md5');
 const Questao = require("../models/Questao");
+const Professor = require("../models/Professor");
 
 module.exports = class QuestaoService {
 
-   async valide(obj) {
-      /* TODO verificar se o professor ta logado
-      if (!obj.login) {
-           return new ResponseBuilder(false, "Login não informado", [])
-      }*/
-      if (!obj.description) {
-         return new ResponseBuilder(false, "Descricao não informada", []);
-      }
-      if (!obj.type) {
-         return new ResponseBuilder(false, "Tipo não informado", []);
-      }
-      if (!obj.type) {
-         return new ResponseBuilder(false, "Tipo não informado", []);
-      }
-      if (obj.type != "write" &&
+    async valide(obj) {
+        /* TODO verificar se o professor ta logado
+        if (!obj.login) {
+             return new ResponseBuilder(false, "Login não informado", [])
+        }*/
+        if (!obj.description) {
+            return new ResponseBuilder(false, "Descricao não informada", []);
+        }
+        if (!obj.type) {
+            return new ResponseBuilder(false, "Tipo não informado", []);
+        }
+        if (!obj.type) {
+            return new ResponseBuilder(false, "Tipo não informado", []);
+        }
+        if (obj.type != "write" &&
             obj.type != "choices") {
-         return new ResponseBuilder(false, "Tipo incorreto", []);
-      }
-   }
+            return new ResponseBuilder(false, "Tipo incorreto", []);
+        }
+        if (!obj.token) {
+            return new ResponseBuilder(false, "Token não informado", []);
+        } else {
+            const professor = await Professor.findOne();
+            const professorToken = md5(professor.login);
 
-   async save(obj) {
-      try {
-           const validations = await this.valide(obj);
-           if (validations) {
-               return validations;
-           }
+            if (obj.token != professorToken) {
+                return new ResponseBuilder(false, "Token inválido", []);
+            }
 
-           const questao = await Questao.create({...obj });
-           return new ResponseBuilder(true, "Salvo com sucesso", questao);
-      } catch (error) {
-           return new ResponseBuilder(false, "Ocorreu um erro ao salvar", []);
-      }
-   }
+            console.log(professor);
+            console.log(professorToken);
+            console.log(obj.token);
+        }
+    }
 
-   async update(id, obj) {
-      try {
-           const validations = await this.valide(obj);
-           if (validations) {
-               return validations;
-           }
+    async save(obj) {
+        try {
+            const validations = await this.valide(obj);
+            if (validations) {
+                return validations;
+            }
 
-           var questao = await Questao.findById(id)
-           questao = obj;
-           await Questao.updateOne(obj);
-           return new ResponseBuilder(true, "Atualizado com sucesso", await Questao.findById(id));
-      } catch (error) {
-           return new ResponseBuilder(false, "Ocorreu um erro ao atualizar", []);
-      }
-   }
+            const questao = await Questao.create({...obj });
+            return new ResponseBuilder(true, "Salvo com sucesso", questao);
+        } catch (error) {
+            return new ResponseBuilder(false, "Ocorreu um erro ao salvar", []);
+        }
+    }
 
-   async getById(id) {
-      try {
-           const questao = await Questao.findById(id);
-           if (questao) {
-               return new ResponseBuilder(true, "Registro retornado com sucesso", questao);
-           }
+    async update(id, obj) {
+        try {
+            const validations = await this.valide(obj);
+            if (validations) {
+                return validations;
+            }
 
-           return new ResponseBuilder(false, "Registro não encontrado", []);
-      } catch (error) {
-           return new ResponseBuilder(false, "Ocorreu um erro ao buscar", []);
-      }
-   }
+            var questao = await Questao.findById(id)
+            questao = obj;
+            await Questao.updateOne(obj);
+            return new ResponseBuilder(true, "Atualizado com sucesso", await Questao.findById(id));
+        } catch (error) {
+            return new ResponseBuilder(false, "Ocorreu um erro ao atualizar", []);
+        }
+    }
 
-   async getAll() {
-      try {
-           return new ResponseBuilder(true, "Registros retornados com sucesso", await Questao.find());
-      } catch (error) {
-           return new ResponseBuilder(false, "Ocorreu um erro ao buscar", []);
-      }
-   }
+    async getById(id) {
+        try {
+            const questao = await Questao.findById(id);
+            if (questao) {
+                return new ResponseBuilder(true, "Registro retornado com sucesso", questao);
+            }
 
-   async delete(id) {
-      try {
-           await Questao.findByIdAndRemove(id);
-           return new ResponseBuilder(true, "Registro excluído com sucesso", []);
-      } catch (error) {
-           return new ResponseBuilder(false, "Ocorreu um erro ao deletar", []);
-      }
-   }
+            return new ResponseBuilder(false, "Registro não encontrado", []);
+        } catch (error) {
+            return new ResponseBuilder(false, "Ocorreu um erro ao buscar", []);
+        }
+    }
+
+    async getAll() {
+        try {
+            return new ResponseBuilder(true, "Registros retornados com sucesso", await Questao.find());
+        } catch (error) {
+            return new ResponseBuilder(false, "Ocorreu um erro ao buscar", []);
+        }
+    }
+
+    async delete(id) {
+        try {
+            await Questao.findByIdAndRemove(id);
+            return new ResponseBuilder(true, "Registro excluído com sucesso", []);
+        } catch (error) {
+            return new ResponseBuilder(false, "Ocorreu um erro ao deletar", []);
+        }
+    }
 
 }
